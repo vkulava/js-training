9775953dccdaba66ed772a87352d168052a56d40
vkulava <128973689+vkulava@users.noreply.github.com>
2025-11-21 15:18:22 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment'
-const current = Date.now() 
-const t = current - (3600 * 24);
-console.log(new Date()); 
-console.log(current); 
-console.log(moment().add(1, 'days').format()); 
-console.log(moment(t).format());
-console.log(Date.now()); 
